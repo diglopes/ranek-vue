df5dc9d683edca719f0aafac8e66e250d6404175
Diego Lopes <diego-chez@hotmail.com>
2020-05-01 20:30:32 -0300

    Stylize header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav>
+      <router-link class="logo" to="/">
+        <img alt="Ranek" src="@/assets/ranek.svg">
+      </router-link>
+      <router-link class="btn" to="/login">Vender / Login</router-link>
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 20px;
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  }
+
+  .logo {
+    padding: 10px 0;
+  }
+
+  .logo img {
+    width: 90px;
+  }
+</style>
