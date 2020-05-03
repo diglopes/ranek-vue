66fd6040003e8c5af0eda2eae4eb5ef51204078b
Diego Lopes <diego-chez@hotmail.com>
2020-05-03 07:58:24 -0300

    Add input fields to CreateAccount

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -1,5 +1,46 @@
 <template>
-  <form>
-    create
-  </form>
+<section>
+  <h2>Crie a sua conta</h2>
+  <transition mode="out-in">
+    <button
+      class="btn"
+      @click="isFormActive = true"
+      v-if="!isFormActive">
+        Criar conta
+    </button>
+    <UserForm v-else>
+      <input type="submit" value="Criar usuário" class="btn"/>
+    </UserForm>
+  </transition>
+</section>
 </template>
+
+
+<script>
+import UserForm from '@/components/UserForm.vue';
+
+export default {
+  data: () => ({
+    isFormActive: false,
+  }),
+  components: {
+    UserForm,
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+</style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form>
+    <label for="name">Nome</label>
+    <input id="name" type="text" v-model="name">
+
+    <label for="email">Nome</label>
+    <input id="email" type="email" v-model="email">
+
+    <label for="password">Senha</label>
+    <input id="password" type="password" v-model="password">
+
+    <label for="cep">Cep</label>
+    <input id="cep" type="text" v-model="Cep">
+
+    <label for="state">Estado</label>
+    <input id="state" type="text" v-model="state">
+
+    <label for="city">Cidade</label>
+    <input id="city" type="text" v-model="city">
+
+    <label for="neighborhood">Bairro</label>
+    <input id="neighborhood" type="text" v-model="neighborhood">
+
+    <label for="street">Rua</label>
+    <input id="street" type="text" v-model="street">
+
+    <label for="number">Número</label>
+    <input id="number" type="text" v-model="number">
+
+    <div class="action-btn">
+      <slot />
+    </div>
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  align-items: center;
+}
+
+.action-btn {
+  grid-column: 2;
+  margin-top: 10px;
+}
+
+.action-btn > * {
+  max-width: 100%;
+}
+</style>
