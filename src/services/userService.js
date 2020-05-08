268618ccd9fda4174134001d6f9b8acecaa44182
Diego Lopes <diego-chez@hotmail.com>
2020-05-08 14:01:46 -0300

    Add update user feature

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,3 +3,5 @@ import httpRequest from './httpRequest';
 export const getUser = ({ email, senha }) => httpRequest.get(`/usuario?email=${email}&senha=${senha}`);
 
 export const createUser = (payload) => httpRequest.post('/usuario', payload);
+
+export const updateUser = (payload) => httpRequest.put(`/usuario/${payload.id}`, payload);
--- src/views/User/UserEdit.vue
+++ src/views/User/UserEdit.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <p>Usuario Editar</p>
+    <UserForm>
+      <button class="btn" @click.prevent="handleUpdateUser">Atualizar usuário</button>
+    </UserForm>
   </div>
 </template>
+
+<script>
+import UserForm from '@/components/UserForm.vue';
+import { updateUser } from '@/services/userService';
+
+export default {
+  components: {
+    UserForm,
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  methods: {
+    handleUpdateUser() {
+      updateUser(this.user).then(() => {
+        this.$store.dispatch('login', this.user);
+        this.$router.push({ name: 'User' });
+      }).catch((err) => console.log(err.response));
+    },
+  },
+};
+</script>
