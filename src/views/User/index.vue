0d62bc7b8e95ad7a109dd1afa846a925adfbcb46
Diego Lopes <diego-chez@hotmail.com>
2020-05-08 10:15:04 -0300

    Set styles to side-nav

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -36,3 +36,47 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.user {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  grid-gap: 30px;
+  padding: 20px;
+}
+
+@media screen and (max-width: 500px){
+  .user {
+    grid-template-columns:  1fr;
+    margin: 0 auto;
+  }
+}
+
+.side-nav a,
+.side-nav button {
+  display: block;
+  padding: 10px;
+  background-color: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.side-nav a.router-link-exact-active,
+.side-nav a:hover,
+.side-nav button:hover {
+  background-color: var(--color-primary);
+  color: #fff;
+}
+
+.side-nav button {
+  border: none;
+  font-size: 1rem;
+  width: 100%;
+  text-align-last: left;
+  font-family: var(--ff);
+  cursor: pointer;
+  color: var(--color-text);
+}
+</style>
